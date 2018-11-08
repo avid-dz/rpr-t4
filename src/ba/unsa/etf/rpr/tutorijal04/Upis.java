bcad967a1a231fb571fa236251e3c39278c573d7
avid-dz <dbrdar1@etf.unsa.ba>
2018-11-08 14:03:44 +0100

    Klasa Upis implementirana

--- src/ba/unsa/etf/rpr/tutorijal04/Upis.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Upis.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal04;
 
 public class Upis {
+
+    private Student student;
+    private Predmet predmet;
+    private PlanStudija planStudija;
+
+    public Upis(Student student, Predmet predmet, PlanStudija planStudija) {
+        this.student = student;
+        this.predmet = predmet;
+        this.planStudija = planStudija;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    public Predmet getPredmet() {
+        return predmet;
+    }
+    public void setPredmet(Predmet predmet) {
+        this.predmet = predmet;
+    }
+    public PlanStudija getPlanStudija() {
+        return planStudija;
+    }
+    public void setPlanStudija(PlanStudija planStudija) {
+        this.planStudija = planStudija;
+    }
 }

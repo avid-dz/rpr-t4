e0ae1cae9de94a354d9771747d1524d916f60c80
avid-dz <dbrdar1@etf.unsa.ba>
2018-11-08 14:34:12 +0100

    Refactoring koda

--- src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Fakultet.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal04;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Fakultet {
+
+    private Set<Predmet> skupSvihPredmeta;
+    private Set<Student> skupSvihStudenata;
+    private Set<Upis> upisani;
+
+    public Fakultet() {
+        skupSvihPredmeta = new TreeSet<>();
+        skupSvihStudenata = new TreeSet<>();
+        upisani = new TreeSet<>();
+    }
+
+    public void dodajPredmetNaFakultet(Predmet predmet) {
+        skupSvihPredmeta.add(predmet);
+    }
+    public void dodajStudentaNaFakultet(Student student) {
+        skupSvihStudenata.add(student);
+    }
+    public void upisi(Student student, Predmet predmet, PlanStudija planStudija) {
+        upisani.add(new Upis(student, predmet, planStudija));
+    }
+
 }

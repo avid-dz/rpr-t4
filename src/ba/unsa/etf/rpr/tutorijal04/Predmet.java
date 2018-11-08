6180d42941253761bdd168e6b3850aa1d6772879
avid-dz <dbrdar1@etf.unsa.ba>
2018-11-08 13:54:28 +0100

    Klasa Predmet popravljena

--- src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
+++ src/ba/unsa/etf/rpr/tutorijal04/Predmet.java
@@ -7,36 +7,23 @@ public class Predmet {
     private String nazivPredmeta;
     private int brojECTS;
     private boolean obavezan;
-    private ArrayList<Student> nizStudenata;
-    private int brojUpisanihStudenata;
 
     public Predmet(String nazivPredmeta, int brojECTS, boolean obavezan) {
         this.nazivPredmeta = nazivPredmeta;
         this.brojECTS = brojECTS;
         this.obavezan = obavezan;
-        nizStudenata = new ArrayList<>();
-        brojUpisanihStudenata = 0;
     }
 
-    public void dodajStudenta(Student student) {
-
-    }
-    public boolean daLiJeObavezan() {
-        return obavezan;
-    }
-    public void ispisiStudentaSaPredmeta(Student student) {
-
-    }
-    public void ispisiSpisakStudenata() {
-
-    }
     public String dajNazivPredmeta() {
         return nazivPredmeta;
     }
     public int dajBrojECTS() {
         return brojECTS;
     }
-    public int dajBrojUpisanihStudenata() {
-        return brojUpisanihStudenata;
+    public boolean daLiJeObavezan() {
+        return obavezan;
+    }
+    public void ispisiSpisakStudenata() {
+
     }
 }
